fe0f8e54ff488f8fcb45876c794d04edb85b04bc
maagjoel <maagjoel1@gmail.com>
2021-09-25 11:59:13 -0400

    added some more endpoints

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from typing import Optional
 from pydantic import BaseModel
 from fastapi import FastAPI
+from trycourier import Courier
 import requests
 import bcrypt
 
+salt = 10  # bcrypt.gensalt()
 API_KEY = 'AIzaSyD1awBg8COVxgukb6PjQK0FYdDCCyAS364'
 
 app = FastAPI()
@@ -16,8 +18,10 @@ class User(BaseModel):
     password: str
     zip_code: str
 
+# need to wait for lazaros code to be able to assign these
 
-class Petition():
+
+class Petition(BaseModel):
     petition_id: str
     for_count: int
     against_count: int
@@ -25,6 +29,33 @@ class Petition():
     body: str
     creator: str
    # image: img  # idk
+
+
+@app.get("/courier/")
+def read_root():
+    url = "https://api.courier.com/send"
+
+    payload = {
+        "event": "EXAMPLE_NOTIFICATION",
+        "recipient": "8ec8c99a-c5f7-455b-9f60-8222b8a27056",
+        "brand": "W50NC77P524K14M5300PGPEK4JMJ",
+        "data": {
+            "name": "Martin Alvarez",
+            "age": 27
+        },
+        "profile": {
+            "phone_number": "7865431215",
+            "email": "malva408@fiu.edu"
+        }
+    }
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.request("POST", url, json=payload, headers=headers)
+
+    return (response.text)
 
 
 @app.get("/zipcode/{zip_code}")
@@ -40,11 +71,17 @@ def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 
-@app.post("/items/")
+@app.post("/signup/")
 async def create_item(item: User):
-    salt = bcrypt.gensalt()
     hash = bcrypt.hashpw(item.password, salt)
     item.password = hash
+    return item
+
+
+@app.post("/login/")
+async def create_item(item: User):
+    # hash = bcrypt.hashpw(item.password, salt)
+    # item.password = hash
     return item
 
 
@@ -60,3 +97,4 @@ def change_count(item: User):
     #     for_count += 1
     # elif is_clicked(against_button):
     #     against_count += 1
+    pass
