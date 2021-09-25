b60eb2ffa85dcc9206477ca8930dbb02c82f4d46
Hermes Bonilla <hboni008@fiu.edu>
2021-09-25 16:29:20 -0400

    refactor user petition for cleaner code

---
+++ backend/db/petitions.py
@@ -0,0 +1,18 @@
+from database_utils import StatusCodes, client_setup
+import datetime
+
+db = client_setup()
+users_collect = db["Petitions"]
+
+
+def create_petition(user_info: dict):
+    if user_info.is_representative:
+        return StatusCodes.UNAUTHORIZED
+
+    date = datetime.datetime.now()
+    date.strftime("%x  %X")
+
+    user_info['created_at'] = date
+
+    users_collect.insert_one(
+        {user_info})
