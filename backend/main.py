c14c6ebab4a33a92567978deb513ba98d7c65214
maagjoel <maagjoel1@gmail.com>
2021-09-25 18:32:30 -0400

    added code for twilio in comment, working on petition functionality

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,25 @@
+import bcrypt
+import requests
 from typing import Optional
 from pydantic import BaseModel
 from fastapi import FastAPI
-import requests
 import bcrypt
+from twilio.rest import Client
+#from trycourier import Courier
+
+# # Your Account SID from twilio.com/console
+# account_sid = "ACff702435d0ed5a311c9c5558e7f171b9"
+# # Your Auth Token from twilio.com/console
+# auth_token = "87f42b26f98d48204c8dbc37f878fe9d"
+
+# client = Client(account_sid, auth_token)
+
+# message = client.messages.create(
+#     to="+17865433977",
+#     from_="+12024172346",
+#     body="Hello from Python(and Martin)!")
+
+# print(message.sid)
 
 salt = bcrypt.gensalt(10)
 API_KEY = 'AIzaSyD1awBg8COVxgukb6PjQK0FYdDCCyAS364'
@@ -14,6 +31,7 @@ class User(BaseModel):
     user_name: str
     password: str
     zip_code: str
+    is_representative: bool
 
 # need to wait for lazaros code to be able to assign these
 
@@ -22,13 +40,43 @@ class Petition(BaseModel):
     petition_id: str
     for_count: int
     against_count: int
-    title: str
-    body: str
+    text_title: str
+    text_body: str
     creator: str
    # image: img  # idk
 
 
 app = FastAPI()
+
+
+# @app.get("/courier/")
+# def read_root():
+#     url = "https://api.courier.com/send"
+
+#     payload = {
+#         "event": "EXAMPLE_NOTIFICATION",
+#         "recipient": "8ec8c99a-c5f7-455b-9f60-8222b8a27056",
+#         "brand": "W50NC77P524K14M5300PGPEK4JMJ",
+#         "data": {
+#             "name": "Martin Alvarez",
+#             "age": 27
+#         },
+#         "profile": {
+#             "phone_number": "7865431215",
+#             "email": "malva408@fiu.edu"
+#         }
+#     }
+#     headers = {
+#         "Accept": "application/json",
+#         "Content-Type": "application/json"
+#     }
+
+#     response = requests.request("POST", url, json=payload, headers=headers)
+
+#     return (response.text)
+
+# works
+
 
 @app.get("/zipcode/{zip_code}")
 def read_root(zip_code: int):
@@ -52,6 +100,13 @@ async def create_item(item: User):
 
 @app.post("/login/")
 async def create_item(item: User):
+    hash = bcrypt.hashpw(b'item.password', salt)
+    item.password = hash
+    return item
+
+
+@app.post("/create_petition/")
+async def create_item(item: Petition):
     hash = bcrypt.hashpw(b'item.password', salt)
     item.password = hash
     return item
