b1738550a6a6a2a3304b944ad5a30c122fdf2abe
Hermes Bonilla <hboni008@fiu.edu>
2021-09-25 07:29:23 -0400

    refactor client setup

--- database_utils.py
+++ database_utils.py
@@ -1,19 +1,29 @@
 from pymongo import MongoClient
 
 
+def client_setup(uri="mongodb+srv://Hermes:ShellHacks2021@cluster0.qf7di.mongodb.net/ShellHacks2021?retryWrites=true&w=majority"):
+    client = MongoClient(uri)
+    return client["ShellHacks2021"]
+
+
 def represenatives_info(rep_info: dict):
-    client = MongoClient(
-        "mongodb+srv://Hermes:ShellHacks2021@cluster0.qf7di.mongodb.net/ShellHacks2021?retryWrites=true&w=majority")
-    db = client["ShellHacks2021"]
+    db = client_setup()
     users_collect = db["Representatives"]  # collections
 
     users_collect.insert_one(rep_info)
 
 
 def petitioner_info(petitioner_info: dict):
-    client = MongoClient(
-        "mongodb+srv://Hermes:ShellHacks2021@cluster0.qf7di.mongodb.net/ShellHacks2021?retryWrites=true&w=majority")
-    db = client["ShellHacks2021"]
+    db = client_setup()
     users_collect = db["Petitioners"]
 
     users_collect.insert_one(petitioner_info)
+
+
+def login_check(username: str):
+
+    # if match return 200 and info
+    # if not, throw error
+
+    # store hashed password
+    # login stuff (find_one())
