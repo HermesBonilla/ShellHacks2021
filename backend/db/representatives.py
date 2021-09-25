d91531a8b89d9e037ab7bf18d72c82a003e563e7
Hermes Bonilla <hboni008@fiu.edu>
2021-09-25 17:17:10 -0400

    add comment update method and comment time

--- backend/db/representatives.py
+++ backend/db/representatives.py
@@ -1,4 +1,5 @@
-from database_utils import client_setup
+from backend.database_utils import StatusCodes, client_setup
+import datetime
 
 db = client_setup()
 users_collect = db["Representatives"]
@@ -6,3 +7,15 @@ users_collect = db["Representatives"]
 
 def representatives_info(rep_info: dict):
     users_collect.insert_one(rep_info)
+
+
+def create_update(user_info: dict):
+    if not user_info.is_representative:
+        return StatusCodes.UNAUTHORIZED
+
+    comment_date = "placeholder"
+    comment_date.strftime("%X, %Y")
+
+    users_collect['comment date'] = comment_date
+
+    users_collect.insert_one({user_info})
